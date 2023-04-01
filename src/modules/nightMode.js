f55d9cc53381dbfdf256baec5566a8d0f8e35c7f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-31 19:01:33 -0600

    Fix linter errors

--- src/modules/nightMode.js
+++ src/modules/nightMode.js
@@ -1,23 +1,23 @@
 export function enableNightMode() {
-    const toggleThemeButton = document.getElementById("toggle-theme");
-    const html = document.documentElement;
-  
-    function toggleNightMode() {
-      if (html.classList.contains("night-mode")) {
-        html.classList.remove("night-mode");
-        localStorage.setItem("nightMode", "disabled");
-      } else {
-        html.classList.add("night-mode");
-        localStorage.setItem("nightMode", "enabled");
-      }
+  const toggleThemeButton = document.getElementById('toggle-theme');
+  const html = document.documentElement;
+
+  function toggleNightMode() {
+    if (html.classList.contains('night-mode')) {
+      html.classList.remove('night-mode');
+      localStorage.setItem('nightMode', 'disabled');
+    } else {
+      html.classList.add('night-mode');
+      localStorage.setItem('nightMode', 'enabled');
     }
-  
-    // Apply the night mode if it was previously enabled
-    if (localStorage.getItem("nightMode") === "enabled") {
-      html.classList.add("night-mode");
-    }
-  
-    toggleThemeButton.addEventListener("click", toggleNightMode);
   }
-  
-  export default enableNightMode;
+
+  // Apply the night mode if it was previously enabled
+  if (localStorage.getItem('nightMode') === 'enabled') {
+    html.classList.add('night-mode');
+  }
+
+  toggleThemeButton.addEventListener('click', toggleNightMode);
+}
+
+export default enableNightMode;
