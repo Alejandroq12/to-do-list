19f05a699e6156fa54e9446b662b0c88fe09c7b4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-31 20:30:46 -0600

    Add styles for night mode

--- src/modules/nightMode.js
+++ src/modules/nightMode.js
@@ -1,23 +1,25 @@
 export function enableNightMode() {
-  const toggleThemeButton = document.getElementById('toggle-theme');
-  const html = document.documentElement;
+  document.addEventListener('DOMContentLoaded', () => {
+    const toggleThemeButton = document.getElementById('toggle-theme');
+    const html = document.documentElement;
 
-  function toggleNightMode() {
-    if (html.classList.contains('night-mode')) {
-      html.classList.remove('night-mode');
-      localStorage.setItem('nightMode', 'disabled');
-    } else {
-      html.classList.add('night-mode');
-      localStorage.setItem('nightMode', 'enabled');
+    function toggleNightMode() {
+      if (html.classList.contains('night-mode')) {
+        html.classList.remove('night-mode');
+        localStorage.setItem('nightMode', 'disabled');
+      } else {
+        html.classList.add('night-mode');
+        localStorage.setItem('nightMode', 'enabled');
+      }
     }
-  }
 
-  // Apply the night mode if it was previously enabled
-  if (localStorage.getItem('nightMode') === 'enabled') {
-    html.classList.add('night-mode');
-  }
+    // Apply the night mode if it was previously enabled
+    if (localStorage.getItem('nightMode') === 'enabled') {
+      html.classList.add('night-mode');
+    }
 
-  toggleThemeButton.addEventListener('click', toggleNightMode);
+    toggleThemeButton.addEventListener('click', toggleNightMode);
+  });
 }
 
 export default enableNightMode;
