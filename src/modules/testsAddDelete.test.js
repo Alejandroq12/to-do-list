8e3fd7593d108c5e2d536c885a76256e95ac6c94
Juan Sanchez <juansan1993@hotmail.com>
2023-04-05 17:21:23 -0500

    Update test file with add and delete tests passing

--- src/modules/testsAddDelete.test.js
+++ src/modules/testsAddDelete.test.js
@@ -1,19 +1,44 @@
 import addTask from './addTask.js';
 
-beforeEach(() => {
-  document.body.innerHTML = `
-    <div>
-      <ul class="todos-ul"></ul>
-    </div>`;
-});
+describe('addTask', () => {
+  let tasks;
+  let inputValue;
+  let taskList;
 
-describe('Add and Delete', () => {
-  test('Add function', () => {
-    const newTask = {
-      description: true,
-      completed: false,
-      index: 1,
-    };
-    expect(newTask.length).toEqual(3);
+  beforeEach(() => {
+    // Set up the mock HTML
+    document.body.innerHTML = `
+      <ul class="todos-ul">
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    `;
+
+    // Initialize variables for each test case
+    tasks = [
+      { description: 'Task 1', completed: false, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: false, index: 3 },
+    ];
+    inputValue = 'New Task';
+    taskList = document.querySelector('.todos-ul');
+  });
+
+  afterEach(() => {
+    // Clean up the mock HTML
+    document.body.innerHTML = '';
+  });
+
+  test('adds exactly one <li> element to the list in the DOM', () => {
+    addTask(tasks, inputValue);
+    expect(taskList.children.length).toBe(4);
+  });
+
+  test('removes exactly one <li> element from the list in the DOM', () => {
+    tasks.pop();
+    const taskToDelete = taskList.children[2];
+    taskList.removeChild(taskToDelete);
+    expect(taskList.children.length).toBe(2);
   });
 });
