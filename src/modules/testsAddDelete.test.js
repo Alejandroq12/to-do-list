963abbc9a91564cf6fb1f5de9fa01fa40f45c884
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-05 09:23:56 -0600

    Add test

---
+++ src/modules/testsAddDelete.test.js
@@ -0,0 +1,10 @@
+import addTask from './addTask.js';
+
+describe('Add and Delete', () => {
+  test('Add function', () => {
+    expect(addTask(tasks, inputValue).toBe())
+  });
+
+  test('Delete function', () => {
+  });
+});
