3f9806174f9a159e42a36c9232f9b61806885d85
Juan Sanchez <juansan1993@hotmail.com>
2023-04-05 10:59:07 -0500

    Add beforeEach test

--- src/modules/testsAddDelete.test.js
+++ src/modules/testsAddDelete.test.js
@@ -1,5 +1,12 @@
 import addTask from './addTask.js';
 
+beforeEach(() => {
+  document.body.innerHTML = `
+    <div>
+      <ul class="todos-ul"></ul>
+    </div>`;
+});
+
 describe('Add and Delete', () => {
   test('Add function', () => {
     const newTask = {
