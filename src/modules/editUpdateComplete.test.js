3461ad077e062739c149a2e17dc9fc9cbafc34b0
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-06 09:41:50 -0600

    Import class and functions

---
+++ src/modules/editUpdateComplete.test.js
@@ -0,0 +1,8 @@
+// The function for editing the task description is on taskElement.js - it is called taskText
+// The function for updating an items completed status is on statusUpdates.js
+// The function for the clear all completed button is on statusUpdates.js
+// I am using an event listener wich uses an arrow function
+
+import TaskElement from './taskElement.js';
+// import { toggleTaskCompletion, clearCompletedTasks } from './statusUpdates.js';
+
